4ad9cc0851db7759c26e2da20e8a14b1d9f243d3
kutakoff <kutakovmax@gmail.com>
2022-02-21 10:33:32 +0300

    Add button "about project." Small changes.

--- app/src/main/java/com/kutakoff/MainActivity.java
+++ app/src/main/java/com/kutakoff/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
         ImageView start = findViewById(R.id.start);
         start.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ChooseActivity.class)));
     }
+
+    public void goToPresentation(View view) {
+        Intent presentationIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/presentation/d/1JNsmC20o93dHImdG71w2i2x0AUVsc-Z6OLlIw9ZwUKM/edit?usp=sharing"));
+        startActivity(presentationIntent);
+    }
+
     public void onBackPressed() {
         new AlertDialog.Builder(this)
                 .setMessage("Вы действительно хотите выйти из приложения?")
